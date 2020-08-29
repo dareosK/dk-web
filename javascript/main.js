46c36d2378e651996bb92b25ba4d9e032346aac5
Dareos Khalili <khalilidareos@gmail.com>
2020-08-30 00:06:44 +0200

    adds timeline(which I think I may not use after all) and make Jquery scroll down method for hero

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+  let scroll = $(window).scrollTop();
+  $(".profile-media img").css({
+    width: ( 100 + scroll/5 ) + "%",
+    opacity: ( 100 - scroll/8 ) + "%"
+  }),
+  $(".intro-text").css({
+    opacity: ( 100 - scroll/2 ) + "%"
+  }),
+  $(".about-me").css({
+    opacity: ( 0 + scroll/9 ) + "%"
+  })
+});
