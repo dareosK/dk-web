2c7e8a1c77a9c6188aedefc0e20ee6e83c5dd121
Dareos Khalili <khalilidareos@gmail.com>
2020-08-30 20:03:19 +0200

    starts ScrollMagic test

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,45 @@
-$(window).scroll(function() {
-  let scroll = $(window).scrollTop();
-  $(".profile-media video").css({
-    width: ( 100 + scroll / 5 ) + "%",
-    opacity: ( 100 - scroll / 8 ) + "%"
-  }),
-  $(".intro-text").css({
-    opacity: ( 100 - scroll / 2 ) + "%"
-  }),
-  $(".about-me").css({
-    opacity: ( 0 + scroll / 9 ) + "%"
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+    let scroll = $(window).scrollTop();
+    $(".profile-media video").css({
+      width: ( 100 + scroll / 5 ) + "%",
+      opacity: ( 100 - scroll / 8 ) + "%"
+    }),
+    $(".intro-text").css({
+      opacity: ( 100 - scroll / 2 ) + "%"
+    }),
+    $(".about-me").css({
+      opacity: ( 0 + scroll / 9 ) + "%"
+    })
+  });
+
+  // init Controller
+  const controller = new ScrollMagic.Controller();
+
+  const workScene = ScrollMagic.Scene({
+    triggerElement: '#gallerie'
   })
+  .setClassToggle('#gallerie', 'scroll-in-up')
+  .addToController();
+
+  // hides the footer when scroll under one window height
+
+  // const hideFooter = () => {
+  // const footer = document.querySelector('#footer');
+  //   if (footer) {
+  //     document.addEventListener('scroll', () => {
+  //       if (window.scrollY >= 4 * window.innerHeight) {
+  //         footer.classList.add('hidden');
+  //       } else {
+  //         footer.classList.remove('hidden');
+  //       }
+  //     });
+  //   }
+  // }
+
+  // hideFooter();
+
+  // export { hideFooter };
+
 });
-
-
-
-// hides the footer when scroll under one window height
-
-// const hideFooter = () => {
-// const footer = document.querySelector('#footer');
-//   if (footer) {
-//     document.addEventListener('scroll', () => {
-//       if (window.scrollY >= 4 * window.innerHeight) {
-//         footer.classList.add('hidden');
-//       } else {
-//         footer.classList.remove('hidden');
-//       }
-//     });
-//   }
-// }
-
-// hideFooter();
-
-// export { hideFooter };
