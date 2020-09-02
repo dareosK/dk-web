d6df509d7d65085040cfe0d1a35553497acd7a8a
Dareos Khalili <khalilidareos@gmail.com>
2020-09-02 14:28:06 +0200

    makes slide wipe work

--- javascript/main.js
+++ javascript/main.js
@@ -4,10 +4,10 @@
 $(document).ready(function() {
 // import simpleParallax from 'simple-parallax-js';
   $(window).scroll(function() {
-    let scroll = $(window).scrollTop();
+    const scroll = $(window).scrollTop();
     $(".profile-media video").css({
       width: ( 100 + scroll / 5 ) + "%",
-      opacity: ( 100 - scroll / 8 ) + "%"
+      opacity: ( 100 - scroll / 15 ) + "%"
     }),
     $(".intro-text").css({
       opacity: ( 100 - scroll / 2 ) + "%"
@@ -18,36 +18,28 @@ $(document).ready(function() {
   });
 });
 
-// function scrollMagic {
-//   // init Controller
-//   const controller = new ScrollMagic.Controller();
+$(function () { // wait for document ready
+    // init
+    const controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        triggerHook: 'onLeave',
+        offset: 700,
+        duration: "100%" // this works just fine with duration 0 as well
+        // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
+        // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
+      }
+    });
 
-//   const workScene = ScrollMagic.Scene({
-//     triggerElement: '#gallerie'
-//   })
-//   .setClassToggle('#gallerie', 'scroll-in-up')
-//   .addToController();
-// };
-// scrollMagic();
+    // get all sections
+    const sections = document.querySelectorAll("section.panel");
 
-// $(function () { // wait for document ready
-//   // init
-//   var controller = new ScrollMagic.Controller();
-
-//   // define movement of panels
-//   var wipeAnimation = new TimelineMax()
-//     .fromTo("section.panel.turqoise", 1, {x: "-100%"}, {x: "0%", ease: Linear.easeNone})  // in from left
-//     .fromTo("section.panel.green",    1, {x:  "100%"}, {x: "0%", ease: Linear.easeNone})  // in from right
-//     .fromTo("section.panel.bordeaux", 1, {y: "-100%"}, {y: "0%", ease: Linear.easeNone}); // in from top
-
-//   // create scene to pin and link animation
-//   new ScrollMagic.Scene({
-//       triggerElement: "#pinContainer",
-//       triggerHook: "onLeave",
-//       duration: "300%"
-//     })
-//     .setPin("#pinContainer")
-//     .setTween(wipeAnimation)
-//     .addIndicators() // add indicators (requires plugin)
-//     .addTo(controller);
-// });
+    // create scene for every slide
+    for (let i=0; i<sections.length; i++) {
+      new ScrollMagic.Scene({
+          triggerElement: sections[i]
+        })
+        .setPin(sections[i], {pushFollowers: false})
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+    }
+  });
