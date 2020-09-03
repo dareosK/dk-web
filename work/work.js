0cc5d93a1428a34bf55d0b0bbc0903f17c770bbb
Dareos Khalili <khalilidareos@gmail.com>
2020-09-03 15:09:16 +0200

    fixes slide issue

--- work/work.js
+++ work/work.js
@@ -1,4 +1,24 @@
 // work.js
 $(function() { // onReady
     const controller = new ScrollMagic.Controller();
+
+    const containerScene = new ScrollMagic.Scene({
+        triggerElement: '#container',
+        duration: 500
+    })
+    .setPin('#block')
+    .addIndicators()
+    .addTo(controller);
+
+  //   const blockTween = new TweenMax.to('#block', 1.5, {
+  //     backgroundColor: 'red'
+  // });
+
+  //   const containerScene = new ScrollMagic.Scene({
+  //     triggerElement: '#container'
+  // })
+  // .setTween(blockTween)
+  // .addIndicators()
+  // .addTo(controller);
+
 });
